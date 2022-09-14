cd2eb3f9874c012c60a36fb8f4b0b5c37081a221
DanielMaherDev <dmdanielmaher@gmail.com>
2022-09-14 00:28:20 +0000

    add basic js to count down and set progress bar on html as time elapses

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+/**
+ * Below is code used to count down from 10, which will be built to se the countdown bar
+ */
+let seconds = 0;
+
+function test() {
+
+    if (seconds < 10) {
+        ++seconds;
+        let countdownBarStage = document.getElementById(`seconds${seconds}`)
+        countdownBarStage.style.backgroundColor="red";
+        console.log(countdownBarStage)
+    } else {
+        return;
+    }
+    console.log(seconds)
+    setTimeout(test, 1000);
+}
+
+test();
