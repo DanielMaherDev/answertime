048372060b939ac62d93b039cc07c08a663c97de
DanielMaherDev <dmdanielmaher@gmail.com>
2022-09-16 19:08:30 +0000

    add main startGame fucntion, add audio file to play on click of start button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,33 @@
+
 /**
  * Below is code used to count down from 10, which will be built to set the countdown bar
  */
-let seconds = 0;
 
-function countdown() {
-    var quiz = document.getElementsByClassName("quiz-container")[0];
-    quiz.classList.add("active");
-    var welcomeScreen = document.getElementById("welcome-screen");
-    welcomeScreen.classList.remove("active");
+function startGame(){
+    let backgroundMusic = new Audio('/assets/audio/background-audio.mp3');
+    backgroundMusic.play();
+        var quiz = document.getElementsByClassName("quiz-container")[0];
+        quiz.classList.add("active");
+    
+        var welcomeScreen = document.getElementById("welcome-screen");
+        welcomeScreen.classList.remove("active");
 
-    if (seconds < 10) {
-        ++seconds;
-        let countdownBarStage = document.getElementById(`seconds${seconds}`)
-        countdownBarStage.style.backgroundColor="red";
-        console.log(countdownBarStage)
-    } else {
-        alert('Game Over!')
-        return;
-    }
-    console.log(seconds)
-    setTimeout(countdown, 1000);
+
+        function countdown() {
+            let seconds = 0;
+            if (seconds < 10) {
+                ++seconds;
+                let countdownBarStage = document.getElementById(`seconds${seconds}`)
+                countdownBarStage.style.backgroundColor="red";
+                console.log(countdownBarStage)
+            } else {
+                alert('Game Over!')
+                return;
+            }
+            console.log(seconds)
+            setTimeout(countdown, 1000);
+        }
 }
 
 const startButton = document.getElementById('start-button');
-startButton.addEventListener('click', countdown)
+startButton.addEventListener('click', startGame)
