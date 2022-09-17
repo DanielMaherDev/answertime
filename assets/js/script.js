f9d08fbc42b7bb1b0a0a36f2c01cd8e61bafb978
DanielMaherDev <dmdanielmaher@gmail.com>
2022-09-17 08:12:01 +0000

    add functionality to call new question when next button is clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,81 +9,91 @@ const answerDivs = document.getElementsByClassName('answer');
 const quiz = document.getElementsByClassName("quiz-container")[0];
 const welcomeScreen = document.getElementById("welcome-screen");
 const questionResult = document.getElementById("question-result");
-let qstnNumber = 0
+const nextQuestion = document.getElementById('next-question');
+let qstnNumber = 0;
 
 
 document.addEventListener("DOMContentLoaded", function () {
-    for (var i = 0; i < answerDivs.length; i++) {
-        (function(index) {
-             answerDivs[index].addEventListener("click", function(){
-                let selectedAnswer =  answerDivs[index];
-                answerSelected(selectedAnswer)
-             })
-        })(i);
-     }
-});
+            for (var i = 0; i < answerDivs.length; i++) {
+                (function (index) {
+                    answerDivs[index].addEventListener("click", function () {
+                        let selectedAnswer = answerDivs[index];
+                        answerSelected(selectedAnswer)
+                    })
+                })(i);
+                nextQuestion.addEventListener("click", function () {
+                        qstnNumber += 1;
+                        newQuestion()
+                }
+                    )
+                }
+            });
 
 
 
-function answerSelected(answer){
-    stopGame = true;
-    questionResult.classList.add("active");
-    quiz.classList.remove("active");
-    console.log(answer)
-    if(answer.getAttribute('id') == questions[qstnNumber].correct){
-        document.getElementById('answered').innerText = answer.innerText    }
-    else{
-        document.getElementById('answered').innerText = 'wrong';
-    }
-    
-}
+        function answerSelected(answer) {
+            stopGame = true;
+            questionResult.classList.add("active");
+            quiz.classList.remove("active");
+            console.log(answer)
+            if (answer.getAttribute('id') == questions[qstnNumber].correct) {
+                document.getElementById('answered').innerText = answer.innerText
+            } else {
+                document.getElementById('answered').innerText = 'wrong';
+            }
 
-function startGame() {
-    /*play audio music in background*/
-    backgroundMusic.play();
-
-    /* hide the welcome screen and show the main quiz container */
-    quiz.classList.add("active");
-
-    welcomeScreen.classList.remove("active");
-
-    newQuestion();
-}
-let stopGame;
-
-function newQuestion() {
-    let seconds = 0; /* declared outside the countdown function as it repeats */
-    /** This is the function which controls the timer, and timeout */
-    function countdown() {
-        if (seconds < 10 && stopGame != true) {
-            ++seconds;
-            let countdownBarStage = document.getElementById(`seconds${seconds}`)
-            countdownBarStage.style.backgroundColor = "red";
-        } else {
-            backgroundMusic.pause();
-            return;
         }
-        setTimeout(countdown, 1000);
-    }
-    countdown();
 
+        function startGame() {
+            /*play audio music in background*/
+            backgroundMusic.play();
 
+            /* hide the welcome screen and show the main quiz container */
+            quiz.classList.add("active");
 
-    /**
-     * The below function will set the question and answer values for the quiz
-     */
-    function setQuestion() {
-        document.getElementById('question').innerHTML = questions[qstnNumber].question;
-        let answerOptions = [];
-        answerOptions = Object.values(questions[qstnNumber])
-        answerOptions = answerOptions.slice(2, 6);
-        for (let i = 0; i < answerDivs.length; i++) {
-            answerDivs[i].innerHTML = answerOptions[i];
+            welcomeScreen.classList.remove("active");
+
+            newQuestion();
         }
-    }
+        let stopGame;
 
-    setQuestion();
-}
+        function newQuestion() {
+            questionResult.classList.remove("active");
+            quiz.classList.add("active");
+            stopGame = false;
+            setQuestion();
 
-const startButton = document.getElementById('start-button');
-startButton.addEventListener('click', startGame)
+
+            let seconds = 0; /* declared outside the countdown function as it repeats */
+            /** This is the function which controls the timer, and timeout */
+            function countdown() {
+                if (seconds < 10 && stopGame != true) {
+                    ++seconds;
+                    let countdownBarStage = document.getElementById(`seconds${seconds}`)
+                    countdownBarStage.style.backgroundColor = "red";
+                } else {
+                    return;
+                }
+                setTimeout(countdown, 1000);
+            }
+            countdown();
+
+
+
+            /**
+             * The below function will set the question and answer values for the quiz
+             */
+            function setQuestion() {
+                document.getElementById('question').innerHTML = questions[qstnNumber].question;
+                let answerOptions = [];
+                answerOptions = Object.values(questions[qstnNumber])
+                answerOptions = answerOptions.slice(2, 6);
+                for (let i = 0; i < answerDivs.length; i++) {
+                    answerDivs[i].innerHTML = answerOptions[i];
+                }
+            }
+
+            setQuestion();
+        }
+
+        const startButton = document.getElementById('start-button'); startButton.addEventListener('click', startGame)
