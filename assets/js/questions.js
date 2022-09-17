10adbb24740d900c23001bf8a19127ec5bc253d1
DanielMaherDev <dmdanielmaher@gmail.com>
2022-09-17 05:06:20 +0000

    create questions.js and add boilerplate template

---
+++ assets/js/questions.js
@@ -0,0 +1,20 @@
+let questions = [
+    {
+      question: "What is the capital of Greece",
+      imgSrc: "img/html.png",
+      choiceA: "Athens",
+      choiceB: "Thesaloniki",
+      choiceC: "Meteora",
+      choiceD: "Mkynos",
+      correct: "A",
+    },
+    {
+      question: "What does STUPID stand for?",
+      imgSrc: "img/html.png",
+      choiceA: "Correct",
+      choiceB: "Wrong",
+      choiceC: "Wrong",
+      choiceD: "Wrong",
+      correct: "A",
+    },
+  ];
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,34 @@
-
 /**
- * Below is code used to count down from 10, which will be built to set the countdown bar
+ * This function runs when the user has selected to start the game. 
  */
-
-function startGame(){
+function startGame() {
+    /*play audio music in background*/
     let backgroundMusic = new Audio('/assets/audio/background-audio.mp3');
     backgroundMusic.play();
-        var quiz = document.getElementsByClassName("quiz-container")[0];
-        quiz.classList.add("active");
-    
-        var welcomeScreen = document.getElementById("welcome-screen");
-        welcomeScreen.classList.remove("active");
-        let seconds = 0;
 
-        countdown();
-        function countdown() {
-            console.log('testcountdown')
-            if (seconds < 10) {
-                ++seconds;
-                let countdownBarStage = document.getElementById(`seconds${seconds}`)
-                countdownBarStage.style.backgroundColor="red";
-                console.log(countdownBarStage)
-            } else {
-                alert('Game Over!')
-                return;
-            }
-            console.log(seconds)
-            setTimeout(countdown, 1000);
+    /* hide the welcome screen and show the main quiz container */
+    var quiz = document.getElementsByClassName("quiz-container")[0];
+    quiz.classList.add("active");
+
+    var welcomeScreen = document.getElementById("welcome-screen");
+    welcomeScreen.classList.remove("active");
+    let seconds = 0; /* declared outside the countdown function as it repeats */
+    /** This is the function which controls the timer, and timeout */
+    countdown();
+    function countdown() {
+        if (seconds < 10) {
+            ++seconds;
+            let countdownBarStage = document.getElementById(`seconds${seconds}`)
+            countdownBarStage.style.backgroundColor = "red";
+            console.log(countdownBarStage)
+        } else {
+            backgroundMusic.pause();
+            alert('Game Over!')
+            return;
         }
+        console.log(seconds)
+        setTimeout(countdown, 1000);
+    }
 }
 
 const startButton = document.getElementById('start-button');
