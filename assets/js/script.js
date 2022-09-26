5c60e7eaaf4d76c8b4340b685985d23cff4d7b52
DanielMaherDev <dmdanielmaher@gmail.com>
2022-09-26 16:54:14 +0000

    add some big fixes for the countDown timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
-import {musicQuestions,
+import {
+    musicQuestions,
     questions
 } from "./questions.js";
 
@@ -17,6 +18,7 @@ const questionResult = document.getElementById("question-result");
 const resultText = document.getElementById("result-text");
 const nextQuestion = document.getElementById('next-question');
 let qstnNumber = 0;
+let currentQuestion = document.getElementById('question-number').innerText
 let seconds = 0;
 const countdownBar = document.getElementsByClassName('progress')
 const startButton = document.getElementById('start-button');
@@ -28,48 +30,74 @@ startButton.addEventListener('click', chooseGame);
 const soundOn = document.getElementById('sound');
 let selectedAnswer = "";
 let correctAnswer;
-soundOn.addEventListener('click', function(){
+let scoreAmount = parseInt(document.getElementById('score-amount').innerText)
+
+soundOn.addEventListener('click', function () {
     backgroundMusic.muted = true;
 })
-for(let i=0; i< buttons.length; i++){
-buttons[i].addEventListener('click', function(){
-    if(buttons[i].classList == 'answer divbtn' && buttons[i].id != correctAnswer){
-        incorrectSound.play();
-    }
-    else if (buttons[i].classList == 'answer divbtn'){
-        correctSound.play();
-    }
-  
-    else{
-        clickSound.play();
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', function () {
+        if (buttons[i].classList == 'answer divbtn' && buttons[i].id != correctAnswer) {
+            incorrectSound.play();
+        } else if (buttons[i].classList == 'answer divbtn') {
+            correctSound.play();
+        } else {
+            clickSound.play();
 
-    }
-});
+        }
+    });
 }
 
-for(let i=0; i< buttons.length; i++){
-    buttons[i].addEventListener('mouseover', function(){
+function newGame() {
+    welcomeScreen.classList.add('active');
+    quiz.classList.remove('active');
+    quiz.innerHTML = `   <div id="">QUESTION <span id="question-number">1</span></div>
+        <div id="progress-bar">
+            <div class="progress" id="seconds10"></div>
+            <div class="progress" id="seconds9"></div>
+            <div class="progress" id="seconds8"></div>
+            <div class="progress" id="seconds7"></div>
+            <div class="progress" id="seconds6"></div>
+            <div class="progress" id="seconds5"></div>
+            <div class="progress" id="seconds4"></div>
+            <div class="progress" id="seconds3"></div>
+            <div class="progress" id="seconds2"></div>
+            <div class="progress" id="seconds1"></div>
+
+        </div>
+        <div class="quiz">
+            <div id="question"></div>
+            <div class="answer divbtn" data-type="answer" id="A"></div>
+            <div class="answer divbtn" data-type="answer" id="B"></div>
+            <div class="answer divbtn" data-type="answer" id="C"></div>
+            <div class="answer divbtn" data-type="answer" id="D"></div>
+        </div>
+
+        <div class="score">Your Score: <span id="score-amount">0</span>/100</div>`
+};
+
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('mouseover', function () {
         optionSound.play();
     });
-    }
+}
 
-function chooseGame(){
-welcomeScreen.classList.add('exit-animation')
-setTimeout(function(){
-    gameTypes.classList.add('active', 'enter-animation')
-welcomeScreen.classList.remove('active')
-}, 1000);
+function chooseGame() {
+    welcomeScreen.classList.add('exit-animation')
+    setTimeout(function () {
+        gameTypes.classList.add('active', 'enter-animation')
+        welcomeScreen.classList.remove('active')
+    }, 1000);
 
 
-for(let g=0; g<gameType.length; g++){
-    gameType[g].addEventListener('click', function(){
-         gameTypeNumber= g;
-        startGame()
-    }
-);
+    for (let g = 0; g < gameType.length; g++) {
+        gameType[g].addEventListener('click', function () {
+            gameTypeNumber = g;
+            startGame()
+        });
     }
 }
-    
+
 
 document.addEventListener("DOMContentLoaded", function () {
     for (var i = 0; i < answerDivs.length; i++) {
@@ -91,26 +119,31 @@ document.addEventListener("DOMContentLoaded", function () {
 
 function answerSelected(answer) {
 
-if(qstnNumber<9){
-    stopGame = true;
-    questionResult.classList.add("active");
-    quiz.classList.remove("active");
-    if (answer.getAttribute('id') == questions[qstnNumber].correct) {
-        document.getElementById('answered').innerText = answer.innerText;
+    if (qstnNumber < 9) {
+        stopGame = true;
+        questionResult.classList.add("active");
+        quiz.classList.remove("active");
+        if (answer.getAttribute('id') == questions[qstnNumber].correct) {
+            document.getElementById('answered').innerText = answer.innerText;
 
-        let scoreAmount = parseInt(document.getElementById('score-amount').innerText)
-        scoreAmount = scoreAmount += (10 - seconds)
-        document.getElementById('score-amount').innerText = scoreAmount;
-        document.getElementById('seconds').innerText = seconds +1;
-        document.getElementById('current-question-score').innerText = 10 - seconds;
+            scoreAmount = scoreAmount += (11 - seconds)
+            document.getElementById('score-amount').innerText = scoreAmount;
+            document.getElementById('seconds').innerText = seconds;
+            document.getElementById('current-question-score').innerText = Math.floor(10.9 - seconds);
 
 
+        } else {
+            scoreAmount = scoreAmount += (11 - seconds)
+            document.getElementById('result-text').innerHTML = 'Incorrect Answer'
+        }
+        seconds = 0;
     } else {
-        document.getElementById('result-text').innerHTML = 'Incorrect Answer'   }
-    seconds = 0;
-    }
-    else{
-        quiz.innerHTML = `game over`
+        document.getElementById('score-amount').innerText = scoreAmount;
+        document.getElementById('current-question-score').innerText = Math.floor(10.9 - seconds);
+        quiz.innerHTML = `game over<br> You scored ${scoreAmount}/100
+        <button id="new-game">New Game</button>`
+
+        document.getElementById('new-game').addEventListener('click', newGame)
     }
 
 }
@@ -118,11 +151,12 @@ if(qstnNumber<9){
 function startGame() {
     gameTypes.classList.remove('enter-animation');
     gameTypes.classList.add('exit-animation');
-    setTimeout(function(){quiz.classList.add('enter-animation', 'active');
-    gameTypes.classList.remove('active');
-    newQuestion();
+    setTimeout(function () {
+        quiz.classList.add('enter-animation', 'active');
+        gameTypes.classList.remove('active');
+        newQuestion();
 
-    },1000)
+    }, 1000)
 
     /*play audio music in background*/
     backgroundMusic.play();
@@ -138,7 +172,7 @@ function newQuestion() {
     quiz.classList.add('active');
     questionResult.classList.remove("active");
     stopGame = false;
-    document.getElementById('question-number').innerText = qstnNumber + 1;
+    currentQuestion = qstnNumber + 1;
     seconds = 0;
     setQuestion();
     countdown();
@@ -146,29 +180,20 @@ function newQuestion() {
     ; /* declared outside the countdown function as it repeats */
     /** This is the function which controls the timer, and timeout */
     function countdown() {
-        if (selectedAnswer == ""){
-            var timer = setTimeout(function(){  
+        if (selectedAnswer == "") {
+            
+            setInterval(function() {
                 if (seconds < 10 && stopGame != true) {
-                ++seconds;
-                let countdownBarStage = document.getElementById(`seconds${seconds}`)
-                countdownBarStage.style.backgroundColor = "red";
-<<<<<<< HEAD
-            } }, 1000)          
-        } 
-     
-=======
-            } }, 1000)
-            
-            
+                    console.log(seconds)
+                    ++seconds;
+                    let countdownBarStage = document.getElementById(`seconds${seconds-1}`)
+                    countdownBarStage.style.backgroundColor = "red";
+                } else
+                    seconds = 0;
+                return;
+            }, 1000)
         }
-      
-    
-        else {
-            console.log(selectedAnswer)
-        clearInterval(timer);
-    return;
-    }
->>>>>>> parent of 8b5d0d6 (begin adding functionality to start new game)
+
     }
 
 
@@ -176,37 +201,34 @@ function newQuestion() {
      * The below function will set the question and answer values for the quiz
      */
     function setQuestion() {
-        
-        if (qstnNumber<10){
-        if(gameTypeNumber == 0){
-        document.getElementById('question').innerHTML = questions[qstnNumber].question; 
-        correctAnswer = questions[qstnNumber].correct;
-        let answerOptions = [];
-        answerOptions = Object.values(questions[qstnNumber])
-        answerOptions = answerOptions.slice(2, 6);
-        for (let i = 0; i < answerDivs.length; i++) {
-            answerDivs[i].innerHTML = answerOptions[i];
+
+        if (qstnNumber < 10) {
+            if (gameTypeNumber == 0) {
+                document.getElementById('question').innerHTML = questions[qstnNumber].question;
+                correctAnswer = questions[qstnNumber].correct;
+                let answerOptions = [];
+                answerOptions = Object.values(questions[qstnNumber])
+                answerOptions = answerOptions.slice(2, 6);
+                for (let i = 0; i < answerDivs.length; i++) {
+                    answerDivs[i].innerHTML = answerOptions[i];
+                }
+            } else {
+                document.getElementById('question').innerHTML = musicQuestions[qstnNumber].question;
+                let answerOptions = [];
+                answerOptions = Object.values(musicQuestions[qstnNumber])
+                answerOptions = answerOptions.slice(2, 6);
+                for (let i = 0; i < answerDivs.length; i++) {
+                    answerDivs[i].innerHTML = answerOptions[i];
+                }
+            }
+        } else {
+            return;
+        }
+        for (let bar of countdownBar) {
+            bar.style.backgroundColor = "green";
         }
     }
-    else{
-        document.getElementById('question').innerHTML = musicQuestions[qstnNumber].question;
-        let answerOptions = [];
-        answerOptions = Object.values(musicQuestions[qstnNumber])
-        answerOptions = answerOptions.slice(2, 6);
-        for (let i = 0; i < answerDivs.length; i++) {
-            answerDivs[i].innerHTML = answerOptions[i];
-    }
-    }
-}
-else{
-    return;
-}
-    for (let bar of countdownBar) {
-        bar.style.backgroundColor = "green";
-    }
-}
-console.log(gameTypeNumber)
+    console.log(gameTypeNumber)
 
     setQuestion();
 }
-
